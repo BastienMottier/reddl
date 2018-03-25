a0f1e692e42bcad26e3065c71e117ba969e7e153
Bastien Mottier <bastien.mottier@gmail.com>
2018-03-25 20:49:48 +0200

    First page scraping work, limit work add class Url to detect image host Add class UserAgent to get a random useragent Update regex class

--- regex.class.php
+++ regex.class.php
@@ -2,6 +2,10 @@
 
 class Regex {
 	private static $subreddit = '/^(?!(?:blog|about|code|advertising|jobs|rules|wiki|contact|buttons|gold|page|help|prefs|user|message|widget))(?:[\w]+)$/i';
+	public static $extractor = [
+		'/^(?:https?:\/\/)?i\.redd\.it\/(\w+\.(?:jpg|png|gif))$/i' => 'https://i.redd.it/%s',
+		'/^(?:https?:\/\/)?(?:i\.)?imgur\.com\/(\w+)(?:\.jpg|png|gif)?$/i' => 'https://i.imgur.com/%s.jpg'
+	];
 
 	public static function __callStatic($name, $arguments)
 	{
